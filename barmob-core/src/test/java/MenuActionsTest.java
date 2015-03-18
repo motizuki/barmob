b4de63c274ef59bf1a26ef0e15dd6df6b99843e8
motizuki <gustavokm90@gmail.com>
2015-03-19 11:57:50 +1300

    Polishing tests

--- barmob-core/src/test/java/MenuActionsTest.java
+++ barmob-core/src/test/java/MenuActionsTest.java
@@ -1,39 +1,12 @@
-import barmob.CoreAppConf;
-import barmob.actions.MenuActions;
-import barmob.persistance.domain.Menu;
 import barmob.persistance.domain.MenuTypes;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 /**
  * Created by gustavokm90 on 3/18/15.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= CoreAppConf.class, loader=AnnotationConfigContextLoader.class)
-public class MenuActionsTest {
-
-    @Autowired
-    MenuActions menuActions;
-
-    int testID;
-
-    @Before
-    public void setUp() throws Exception {
-        System.out.println("Preparing tests");
-        Menu menu = menuActions.createMenu("Pasta", 45.0, true, "Cool pasta", MenuTypes.MAIN);
-        testID = menu.getId();
-        assertEquals("Testing createMenu: ", "Pasta", menu.getName());
-
-    }
-
+public class MenuActionsTest extends TestActions{
 
     @Test
     public void testMenuActions() {
@@ -42,10 +15,5 @@ public class MenuActionsTest {
         assertEquals("Testing updateMenu: ", true, menuActions.updateMenu(23, "availability", false));
     }
 
-    @After
-    public void tearDown() throws Exception {
-        System.out.println("Finalizing tests");
-        assertEquals("Testing deleteMenu: ", true, menuActions.deleteMenu(testID));
-    }
 
 }
--- barmob-core/src/test/java/TableActionsTest.java
+++ barmob-core/src/test/java/TableActionsTest.java
@@ -1,55 +1,20 @@
-import barmob.CoreAppConf;
-import barmob.actions.MenuActions;
-import barmob.actions.TableActions;
-import barmob.persistance.domain.Menu;
-import barmob.persistance.domain.MenuTypes;
-import barmob.persistance.domain.TableStatus;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
-import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 /**
  * Created by gustavokm90 on 3/18/15.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= CoreAppConf.class, loader=AnnotationConfigContextLoader.class)
-public class TableActionsTest {
-
-    @Autowired
-    TableActions tableActions;
-
-    int idTest;
-
-    @Before
-    public void setUp() throws Exception {
-        System.out.println("Preparing tests");
-        idTest = tableActions.createTable(1,"123",TableStatus.FREE).getId();
-
-    }
+public class TableActionsTest extends TestActions {
 
 
     @Test
     public void testTableActions() {
 
-        assertTrue("Start Table: ",tableActions.startTable(idTest));
-        assertTrue("Start Table: ", tableActions.closeTable(idTest));
-        assertTrue("Start Table: ", tableActions.resetTable(idTest));
+        assertTrue("Start Table: ",tableActions.startTable(idTestTable));
+        assertTrue("Start Table: ", tableActions.closeTable(idTestTable));
+        assertTrue("Start Table: ", tableActions.resetTable(idTestTable));
 
 
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        System.out.println("Finalizing tests");
-        tableActions.deleteTable(idTest);
     }
 
 }
